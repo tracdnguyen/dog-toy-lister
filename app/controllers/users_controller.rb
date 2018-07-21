accf21f629a64a18971bed87196f6d546e06d854
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 19:19:31 +0000

    added route for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
+  use Rack::Flash
+
+  get '/signup' do
+    if is_logged_in?(session)
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/'
+    end
+  end
+
+
 
 end
