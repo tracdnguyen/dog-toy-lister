b31d938e6d41d9c031641034ed98d899d7d86ecd
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 20:47:38 +0000

    post route for login authenticates user to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,5 +34,14 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/toys"
+    else
+      redirect to '/signup'
+    end
+  end
 
 end
