9a7a01e2c468ff78a06c73bc1e1519a683c2e3a7
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 21:22:02 +0000

    post route for toys creates new toy if user is logged ing

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -16,5 +16,23 @@ class ToysController < ApplicationController
     end
   end
 
+  post '/toys' do
+    if logged_in?
+      if params[:name] == "" || params[:url] == ""
+        flash[:error] = "Please enter both a name and url for your new favorite toy!"
+        redirect to "/toys/new"
+      else
+        @toy = current_user.toys.build(name: params[:name], url: params[:url])
+        if @toy.save
+          redirect to "/toys/#{@toy.id}"
+        else
+          redirect to "/toys/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
 
 end
