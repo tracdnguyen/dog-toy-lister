588253988ab7e9b0e186c43ab6f788480036f34e
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 19:00:56 +0000

    set up environment

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require 'bundler/setup'
+Bundler.require
+require 'rack-flash'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
