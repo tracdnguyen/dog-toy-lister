96737a0a1c455914e49f33710a4091ed2bab7695
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 20:07:39 +0000

    after user sign up, they're now redirected to their ome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,16 @@ class UsersController < ApplicationController
     erb :'users/show'
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      flash[:error] = "Please sign up with both a username and password."
+      redirect '/users/create_user'
+    else
+      @user = User.new(:username => params[:username], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to "users/#{@user.id}"
+    end
+  end
 
 end
