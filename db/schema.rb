48f6ed03560ee42eb0c6764d2d81fe8a0331b839
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 21:54:31 +0000

    added migration to create user_id column in toys table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_190651) do
+ActiveRecord::Schema.define(version: 2018_07_21_215123) do
 
   create_table "toys", force: :cascade do |t|
     t.string "name"
     t.string "url"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
