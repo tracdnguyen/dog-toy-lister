8b3ae7d74614ab704e82e21e593858dbbc2cb855
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 19:32:46 +0000

    added individual user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < ApplicationController
   use Rack::Flash
 
   get '/signup' do
-    if is_logged_in?(session)
+    if logged_in?
       erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
     else
       redirect to '/'
     end
   end
 
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
 
 
 end
