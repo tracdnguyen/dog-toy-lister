696036ff5a44b9996f3eff89058a9f86c9a13b2b
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-21 20:48:53 +0000

    logout route ends session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,4 +44,12 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
