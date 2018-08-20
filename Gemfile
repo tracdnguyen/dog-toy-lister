9a53f0330102273caa996781471353f66ff0079f
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-20 16:34:01 +0000

    set up bootstrap gem and basic layout page

--- Gemfile
+++ Gemfile
@@ -13,3 +13,4 @@ gem 'bcrypt'
 gem "tux"
 gem 'rack'
 gem 'rack-flash3'
+gem 'sinatra-twitter-bootstrap', :require => 'sinatra/twitter-bootstrap'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::Twitter::Bootstrap::Assets
 
   configure do
     set :public_folder, 'public'
